b0ac232151966b9784a520f717220c4dcfeefbd5
Bill Pemberton <wfp5p@worldbroken.com>
2016-09-28 11:33:14 -0400

    Update nukedb_test to use time.Time params instead of integer

--- nukedb/nukedb_test.go
+++ nukedb/nukedb_test.go
@@ -9,6 +9,9 @@ import (
 )
 
 func TestCreate(t *testing.T) {
+
+	someDate := time.Now()
+
 	tempDir, e1 := ioutil.TempDir("", "nukedb")
 	if e1 != nil {
 		t.Fatal(e1)
@@ -21,12 +24,12 @@ func TestCreate(t *testing.T) {
 		t.Error("it failed")
 	}
 
-	n.Insert("128.143.12.12", 1985, 5)
+	n.Insert("128.143.12.12", someDate, 5)
 
 	var r NukeRecord
 	r, err = n.GetInfo("128.143.12.12")
 
-	if err != nil || r.Expire != 1985 || r.Blocks != 5 {
+	if err != nil || r.Expire != someDate.Unix() || r.Blocks != 5 {
 		t.Errorf("GetInfo failed %v %v\n", err, r)
 	}
 
@@ -35,29 +38,29 @@ func TestCreate(t *testing.T) {
 		t.Errorf("GetInfo returned something when it shouldn't %v %v", err, r)
 	}
 
-	err = n.InsertExpire("128.143.12.12", 1990)
+	err = n.InsertExpire("128.143.12.12", someDate)
 	if err != nil {
 		t.Errorf("InsertExpire failed %v\n", err)
 	}
 
 	r, err = n.GetInfo("128.143.12.12")
 
-	if err != nil || r.Expire != 1990 || r.Blocks != 6 {
+	if err != nil || r.Expire != someDate.Unix() || r.Blocks != 6 {
 		t.Errorf("GetInfo after update failed %v %v\n", err, r)
 	}
 
-	err = n.InsertExpire("128.143.5.7", 1990)
+	err = n.InsertExpire("128.143.5.7", someDate)
 	if err != nil {
 		t.Errorf("InsertExpire failed %v\n", err)
 	}
 
 	r, err = n.GetInfo("128.143.5.7")
 
-	if err != nil || r.Expire != 1990 || r.Blocks != 1 {
+	if err != nil || r.Expire != someDate.Unix() || r.Blocks != 1 {
 		t.Errorf("GetInfo after update2 failed %v %v\n", err, r)
 	}
 
-	ips, e3 := n.GetExpires(time.Unix(1000000,0))
+	ips, e3 := n.GetExpires(someDate.AddDate(0,0,1))
 	if e3 != nil || len(ips) != 2 {
 		t.Errorf("GetExpires failed %v %v\n", ips, e3)
 	}
